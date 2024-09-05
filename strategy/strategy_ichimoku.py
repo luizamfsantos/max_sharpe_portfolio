b6acedc30ec8ff9121102ad73d3aaaaf5e99d237
luizamfsantos <luiza.marques_@hotmail.com>
2024-09-05 19:40:55 -0300

    refactor: format script

--- strategy/strategy_ichimoku.py
+++ strategy/strategy_ichimoku.py
@@ -5,7 +5,11 @@ from simulator.strategy_interface import StrategyInterface
 from data_market.get_retornos_sp import get_retornos_sp
 from strategy.initial_weights import get_uniform_noneg
 
-def calculate_conversion_line(data:pd.DataFrame,rolling_periods:int=9) -> pd.Series:
+
+def calculate_conversion_line(
+    data: pd.DataFrame,
+    rolling_periods: int = 9
+) -> pd.Series:
     """
     Calculate the conversion line (tenkan_sen) 
     of the Ichimoku strategy. Using the formula:
@@ -19,11 +23,15 @@ def calculate_conversion_line(data:pd.DataFrame,rolling_periods:int=9) -> pd.Ser
     below the Tenkan-sen, it suggests a bearish (decreasing)
     trend.
     """
-    if ['high','low'] not in data.columns:
+    if ['high', 'low'] not in data.columns:
         raise ValueError("Data must contain 'high' and 'low' columns")
     return (data['high'].rolling(rolling_periods).max() + data['low'].rolling(rolling_periods).min()) / 2
 
-def calculate_baseline(data:pd.DataFrame,rolling_periods:int=26) -> pd.Series:
+
+def calculate_baseline(
+    data: pd.DataFrame, 
+    rolling_periods: int = 26
+    ) -> pd.Series:
     """
     Calculate the base line (kijun_sen)
     of the Ichimoku strategy. Using the formula:
@@ -37,14 +45,15 @@ def calculate_baseline(data:pd.DataFrame,rolling_periods:int=26) -> pd.Series:
     of the market and to identify potential support or 
     resistance levels.
     """
-    return calculate_conversion_line(data,rolling_periods)
+    return calculate_conversion_line(data, rolling_periods)
+
 
 def calculate_leading_span_A(
-    data:pd.DataFrame,
-    baseline_rolling_periods:int|None=None,
-    conversion_rolling_periods:int|None=None,
-    future_periods:int=52
-    ) -> pd.Series:
+    data: pd.DataFrame,
+    baseline_rolling_periods: int | None = None,
+    conversion_rolling_periods: int | None = None,
+    future_periods: int = 52
+) -> pd.Series:
     """
     Calculate the leading span A (senkou_span_A)
     of the Ichimoku strategy. 
@@ -54,16 +63,18 @@ def calculate_leading_span_A(
     It is often plotted 52 periods ahead of the current
     """
     if 'baseline' not in data.columns:
-        data['baseline'] = calculate_baseline(data,baseline_rolling_periods)
+        data['baseline'] = calculate_baseline(data, baseline_rolling_periods)
     if 'conversion_line' not in data.columns:
-        data['conversion_line'] = calculate_conversion_line(data,conversion_rolling_periods)
+        data['conversion_line'] = calculate_conversion_line(
+            data, conversion_rolling_periods)
     return ((data['baseline'] + data['conversion_line']) / 2).shift(future_periods)
 
+
 def calculate_leading_span_B(
-    data:pd.DataFrame, 
-    rolling_periods:int=26, 
-    future_periods:int=30
-    ) -> pd.Series:
+    data: pd.DataFrame,
+    rolling_periods: int = 26,
+    future_periods: int = 30
+) -> pd.Series:
     """
     Calculate the leading span B (senkou_span_B)
     of the Ichimoku strategy. 
@@ -72,11 +83,15 @@ def calculate_leading_span_B(
     and projecting them forward. 
     It is often plotted 52 periods ahead of the current
     """
-    if ['high','low'] not in data.columns:
+    if ['high', 'low'] not in data.columns:
         raise ValueError("Data must contain 'high' and 'low' columns")
     return ((data['high'].rolling(rolling_periods).max() + data['low'].rolling(rolling_periods).min()) / 2).shift(future_periods)
 
-def calculate_lagging_span(data:pd.DataFrame, lagging_periods:int=30) -> pd.Series:
+
+def calculate_lagging_span(
+    data: pd.DataFrame, 
+    lagging_periods: int = 30
+    ) -> pd.Series:
     """ 
     Calculate the lagging span (chikou_span)
     of the Ichimoku strategy.
@@ -84,15 +99,16 @@ def calculate_lagging_span(data:pd.DataFrame, lagging_periods:int=30) -> pd.Seri
     """
     if 'close' not in data.columns:
         raise ValueError("Data must contain 'close' column")
-    lagging_periods = -1* abs(lagging_periods)
+    lagging_periods = -1 * abs(lagging_periods)
     return data['close'].shift(lagging_periods)
 
+
 def calculate_cloud(
-    data: pd.DataFrame, 
+    data: pd.DataFrame,
     baseline_rolling_periods: int | None = None,
     conversion_rolling_periods: int | None = None,
     future_periods: int | None = None
-    ) -> pd.Series[int]:
+) -> pd.Series[int]:
     """
     Calculate whether the prices are above, below, 
     or within the cloud.
@@ -102,21 +118,24 @@ def calculate_cloud(
     0 if the close price is within the cloud. # flat
     """
     if 'leading_span_A' not in data.columns:
-        data['leading_span_A'] = calculate_leading_span_A(data,baseline_rolling_periods,conversion_rolling_periods,future_periods)
+        data['leading_span_A'] = calculate_leading_span_A(
+            data, baseline_rolling_periods, conversion_rolling_periods, future_periods)
     if 'leading_span_B' not in data.columns:
-        data['leading_span_B'] = calculate_leading_span_B(data,baseline_rolling_periods,future_periods)
-    
+        data['leading_span_B'] = calculate_leading_span_B(
+            data, baseline_rolling_periods, future_periods)
+
     upper_span = data[['leading_span_A', 'leading_span_B']].max(axis=1)
     lower_span = data[['leading_span_A', 'leading_span_B']].min(axis=1)
-    
+
     conditions = [
         (data['close'] > upper_span),
         (data['close'] < lower_span),
         (data['close'].between(lower_span, upper_span))
     ]
     choices = [1, -1, 0]
-    
+
     return pd.Series(np.select(conditions, choices, default=0), index=data.index)
+
 
 class IchimokuStrategy(StrategyInterface):
 
