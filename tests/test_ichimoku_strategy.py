33da20b0ff3663307bc9db0cc311f5cb5bf7e8d8
luizamfsantos <luiza.marques_@hotmail.com>
2024-09-07 18:20:50 -0300

    test basic ichimoku functions work

--- tests/test_ichimoku_strategy.py
+++ tests/test_ichimoku_strategy.py
@@ -1,4 +1,9 @@
-from strategy.strategy_ichimoku import calculate_conversion_line
+from strategy.strategy_ichimoku import (
+    calculate_conversion_line, 
+    calculate_baseline, 
+    calculate_leading_span_A, 
+    calculate_leading_span_B
+)
 import pandas as pd
 from pathlib import Path
 from unittest import TestCase
@@ -9,9 +14,22 @@ class IchimokuTest(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.data = load_data()
+        cls.ticker = 'PETR4'
+        cls.stock_data = cls.data['prices_complete'].copy()
+        cls.stock_data = cls.stock_data[cls.stock_data['Symbol'] == cls.ticker].copy().sort_values('Date').reset_index(drop=True)
 
     def test_calculate_conversion_line(self):
-        data = self.data['stocks'].copy()
-        stock_data = data['AAPL']
-        stock_data['conversion_line'] = calculate_conversion_line(stock_data)
-        print(stock_data)
+        self.stock_data['conversion_line'] = calculate_conversion_line(self.stock_data)
+        self.assertTrue('conversion_line' in self.stock_data.columns)
+    
+    def test_calculate_baseline(self):
+        self.stock_data['baseline'] = calculate_baseline(self.stock_data)
+        self.assertTrue('baseline' in self.stock_data.columns)
+
+    def test_calculate_leading_span_A(self):
+        self.stock_data['leading_span_A'] = calculate_leading_span_A(self.stock_data)
+        self.assertTrue('leading_span_A' in self.stock_data.columns)
+
+    def test_calculate_leading_span_B(self):
+        self.stock_data['leading_span_B'] = calculate_leading_span_B(self.stock_data)
+        self.assertTrue('leading_span_B' in self.stock_data.columns)
