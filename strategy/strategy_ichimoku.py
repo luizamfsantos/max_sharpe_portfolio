dc607b33749503558b7c1c5b54cfb41e84d1dff1
luizamfsantos <luiza.marques_@hotmail.com>
2024-09-06 21:52:22 -0300

    feat: cloud condition

--- strategy/strategy_ichimoku.py
+++ strategy/strategy_ichimoku.py
@@ -8,7 +8,7 @@ from strategy.initial_weights import get_uniform_noneg
 
 def calculate_conversion_line(
     data: pd.DataFrame,
-    rolling_periods: int = 9
+    rolling_periods: int | None = 9
 ) -> pd.Series:
     """
     Calculate the conversion line (tenkan_sen) 
@@ -23,6 +23,8 @@ def calculate_conversion_line(
     below the Tenkan-sen, it suggests a bearish (decreasing)
     trend.
     """
+    if rolling_periods is None:
+        rolling_periods = 9
     if ['high', 'low'] not in data.columns:
         raise ValueError("Data must contain 'high' and 'low' columns")
     return (data['high'].rolling(rolling_periods).max() + data['low'].rolling(rolling_periods).min()) / 2
@@ -30,7 +32,7 @@ def calculate_conversion_line(
 
 def calculate_baseline(
     data: pd.DataFrame,
-    rolling_periods: int = 26
+    rolling_periods: int | None = 26
 ) -> pd.Series:
     """
     Calculate the base line (kijun_sen)
@@ -45,6 +47,8 @@ def calculate_baseline(
     of the market and to identify potential support or 
     resistance levels.
     """
+    if rolling_periods is None:
+        rolling_periods = 26
     return calculate_conversion_line(data, rolling_periods)
 
 
@@ -62,6 +66,8 @@ def calculate_leading_span_A(
     and projecting them forward. 
     It is often plotted 52 periods ahead of the current
     """
+    if future_periods is None:
+        future_periods = 52
     if 'baseline' not in data.columns:
         data['baseline'] = calculate_baseline(data, baseline_rolling_periods)
     if 'conversion_line' not in data.columns:
@@ -83,6 +89,10 @@ def calculate_leading_span_B(
     and projecting them forward. 
     It is often plotted 52 periods ahead of the current
     """
+    if rolling_periods is None:
+        rolling_periods = 26
+    if future_periods is None:
+        future_periods = 30
     if ['high', 'low'] not in data.columns:
         raise ValueError("Data must contain 'high' and 'low' columns")
     return ((data['high'].rolling(rolling_periods).max() + data['low'].rolling(rolling_periods).min()) / 2).shift(future_periods)
@@ -99,9 +109,52 @@ def calculate_lagging_span(
     """
     if 'close' not in data.columns:
         raise ValueError("Data must contain 'close' column")
+    if lagging_periods is None:
+        lagging_periods = 30
     lagging_periods = -1 * abs(lagging_periods)
     return data['close'].shift(lagging_periods)
 
+def get_entry_signal(
+    data: pd.DataFrame
+) -> None:
+    if signal not in data.columns:
+        data['signal'] = np.nan
+
+    # Prices are above the cloud
+    condition_1 = (data.close > data.leading_span_A) & (data.close > data.leading_span_B)
+
+    # Leading Span A (senkou_span_A) is rising above the leading span B (senkou_span_B)
+    condition_2 = (data.leading_span_A > data.leading_span_B)
+
+    # Conversion Line (tenkan_sen) moves above Base Line (kijun_sen)
+    condition_3 = (data.conversion_line > data.base_line)
+
+    # Combine the conditions and store in the signal column 1
+    data.loc[condition_1 & condition_2 & condition_3, 'signal'] = 1
+
+def get_exit_signal(
+    data: pd.DataFrame
+) -> None:
+    if signal not in data.columns:
+        data['signal'] = np.nan
+
+    # Prices are below the cloud
+    condition_1 = (df.close < df.leading_span_A) & (df.close < df.leading_span_B)
+
+    # Leading Span A (senkou_span_A) is falling below
+    # the leading span B (senkou_span_B)
+    condition_2 = (df.leading_span_A < df.leading_span_B)
+
+    # Conversion Line (tenkan_sen) moves below Base Line (kijun_sen)
+    condition_3 = (df.conversion_line < df.base_line)
+
+    # Combine the conditions and store in the signal
+    # column 0 when all the conditions are true
+    df.loc[condition_1 & condition_2 & condition_3, 'signal'] = 0
+
+    # Fill missing values in the 'signal' column using
+    # forward fill (ffill) method
+    df.signal.fillna(method='ffill', inplace=True)
 
 def calculate_cloud(
     data: pd.DataFrame,
@@ -127,15 +180,33 @@ def calculate_cloud(
     upper_span = data[['leading_span_A', 'leading_span_B']].max(axis=1)
     lower_span = data[['leading_span_A', 'leading_span_B']].min(axis=1)
 
-    conditions = [
-        (data['close'] > upper_span),
-        (data['close'] < lower_span),
-        (data['close'].between(lower_span, upper_span))
-    ]
-    choices = [1, -1, 0]
+    # Prices are above/below or within the cloud
+    condition_price_above = (data.close > data.leading_span_A) & (
+        data.close > data.leading_span_B)
+    condition_price_below = data.close < data.leading_span_A & (
+        data.close < data.leading_span_B)
+    data['cloud_condition_1'] = np.where(condition_price_above, 1, np.where(
+        condition_price_below, -1, 0))
 
-    return pd.Series(np.select(conditions, choices, default=0), index=data.index)
+    # Leading Span A is above/below the leading span B
+    condition_leading_span_A_above = data.leading_span_A > data.leading_span_B
+    condition_leading_span_A_below = data.leading_span_A < data.leading_span_B
+    data['cloud_condition_2'] = np.where(condition_leading_span_A_above & condition_price_above, 1, np.where(
+        condition_leading_span_A_below & condition_price_below, -1, 0))
 
+    # Conversion Line moves above/below the Base Line
+    condition_conversion_above = data.conversion_line > data.base_line
+    condition_conversion_below = data.conversion_line < data.base_line
+    data['cloud_condition_3'] = np.where(condition_conversion_above & condition_price_above, 1, np.where(
+        condition_conversion_below & condition_price_below, -1, 0))
+
+    # Combine the conditions
+    data['cloud_combined'] = data['cloud_condition_1'] + \
+        data['cloud_condition_2'] + data['cloud_condition_3']
+    data['cloud'] = np.where(data['cloud_combined'] == 3, 1, np.where(
+        data['cloud_combined'] == -3, -1, 0))
+    
+    return data['cloud']
 
 class IchimokuStrategy(StrategyInterface):
 
@@ -178,18 +249,22 @@ class IchimokuStrategy(StrategyInterface):
         """
         Implement the Ichimoku strategy.
         """
-        stocks_buy = _buy_stocks(data, t) # create a portfolio of stocks to buy
+        stocks_buy = _buy_stocks(
+            data, t)  # create a portfolio of stocks to buy
 
-        # TODO: balance the portfolio 
+        # TODO: balance the portfolio
         return stocks
+
 
 class MarketCondition():
 
     def __init__(self, data: pd.DataFrame, index: int):
-        self.data = data.iloc[:index,:] # only look at the data up to the current index
+        # only look at the data up to the current index
+        self.data = data.iloc[:index, :]
         self.index = index
         self.stock_list = self._get_stocks()
-        self.cloud = {stock: self._calculate_stock_cloud_condition(stock) for stock in self.stock_list}
+        self.cloud = {stock: self._calculate_stock_cloud_condition(
+            stock) for stock in self.stock_list}
 
     def _get_stocks(self) -> list[str]:
         return [col for col in self.data.columns if col not in ['Date']]
@@ -203,4 +278,4 @@ class MarketCondition():
         conversion_line = calculate_conversion_line(stock_data)
         leading_span_A = calculate_leading_span_A(stock_data)
         leading_span_B = calculate_leading_span_B(stock_data)
-        return calculate_cloud(stock_data)
+        #return calculate_cloud(stock_data)
